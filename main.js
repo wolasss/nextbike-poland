c4e900806e4047669882c67f5ada4541c24091d0
Adam Wolski <wolas.abc@gmail.com>
2015-04-29 00:11:30 +0200

    debug lang

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ if(Meteor.isClient) {
 	Template.nbHome.rendered = function() {
 		Deps.autorun(function(){
 			var city = ReactiveStore.get("nbCity");
-			var lang = TAPi18n.getLanguage();			
+			var lang = TAPi18n.getLanguage();	
+			
+			console.log(city, lang);
+
 			if(city && lang) {
 				NB.Cities.load(lang, city);
 			}
